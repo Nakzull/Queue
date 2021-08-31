7d13366d931d087d85ba3bb9fd23e8c353bf97db
Nakzull <andr25f4@zbc.dk>
2021-08-31 10:53:48 +0200

    Add project files.

---
+++ Queue/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Customer(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
